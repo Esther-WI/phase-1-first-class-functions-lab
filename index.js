9438dbbf8f25b64e9e3484ace6b6ce9fdf522172
Esther Wangari <essywangari06@gmail.com>
2025-03-11 22:16:13 +0300

    Completed phase

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers){
+    return drivers.slice(0,2)
+}
+console.log(returnFirstTwoDrivers(["Antonia", "Nuru", "Amari", "Mo"]));
+
+const returnLastTwoDrivers = function (drivers){
+    return drivers.slice(2,4)
+}
+console.log(returnLastTwoDrivers(["Antonia", "Nuru", "Amari", "Mo"]));
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(x){
+    return function(fare)
+    {return x*fare}
+
+    }
+
+    const fareDoubler = createFareMultiplier(2);
+    const fareTripler = createFareMultiplier(3);
+
+    function selectDifferentDrivers(drivers, driverSelectorFunction){
+        return driverSelectorFunction(drivers);
+    }
